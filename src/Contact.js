e6fceddc5780852d079ae67c020941d38d063b61
Kris Coulson <kriscoulson@gmail.com>
2016-08-31 15:07:40 -0700

    add about, home and contact and make my mentors work

---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Contact = () =>
+  <div id="list">
+    <h1>Questions?</h1>
+    <div class="copy" style={{textAlign: 'center', lineHeight: '20px'}}>
+      If you need assistance with the app or are interested in joining the Reframe team, please contact us at  luu.ashley@gmail.com.
+    </div>
+  </div>
+
+export default Contact
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './css/home.css'
+
+const Home = () =>
+  <div className="home-wrapper">
+    <div className="home-header">reframe</div>
+  </div>
+
+export default Home
